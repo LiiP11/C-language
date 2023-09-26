507a32435e38ad7bcf1c11505ad82a3ccf857637
Eliezer Pimenta <eliezer.a.pimenta@gmail.com>
2023-09-26 18:20:27 -0300

    Exercises in C language: putting names in alphabetical order
    In this updated code:
    Instead of a fixed-size array, we use a dynamic array of strings (char** names) and allocate memory for it using malloc.
    This code is more flexible and can handle any number of names specified by the user. It also performs proper memory allocation and deallocation to avoid memory-related issues.

--- alphabetical.c
+++ alphabetical.c
@@ -1,33 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main() {
-	int i, j;
-	int len = 3;
-	char name[len][20], aux[20];
+    int i, j;
+    int len;
 
-	printf("Type three random names:\n\n");
+    printf("Enter the number of names: ");
+    scanf("%d", &len);
 
-	for (i = 0; i < len; i++) {
-		printf("%d° name: ",i + 1);
-		scanf("%s", name[i]);
-	}
+    // Consume the newline character left in the input buffer
+    while (getchar() != '\n');
 
-	for (i = 0; i < len; i++) { 
-		for (j = 1; j < len; j++) {
-			if (strcmp(name[j - 1], name[j]) > 0) {
-				strcpy(aux, name[j - 1]);
-				strcpy(name[j - 1], name[j]);
-				strcpy(name[j], aux);
-			}
-		}
-	}
+    // Dynamically allocate memory for an array of strings
+    char** names = (char**)malloc(len * sizeof(char*));
 
-	printf("\nAphabetical order:\n\n");
+    if (names == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
 
-	for (i = 0; i < len; i++) {
-		printf("%d° name: %s\n", i + 1, name[i]);
-	}
+    for (i = 0; i < len; i++) {
+        names[i] = (char*)malloc(256 * sizeof(char)); // Assuming a maximum of 255 characters per name
+        if (names[i] == NULL) {
+            fprintf(stderr, "Memory allocation failed\n");
+            return 1;
+        }
 
-	return 0;
+        printf("%d° name: ", i + 1);
+        fgets(names[i], 255, stdin);
+
+        // Remove the newline character at the end of the input
+        names[i][strcspn(names[i], "\n")] = '\0';
+    }
+
+    // Perform bubble sort on the names
+    for (i = 0; i < len - 1; i++) {
+        for (j = 0; j < len - i - 1; j++) {
+            if (strcmp(names[j], names[j + 1]) > 0) {
+                char* temp = names[j];
+                names[j] = names[j + 1];
+                names[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("\nAlphabetical order:\n\n");
+
+    for (i = 0; i < len; i++) {
+        printf("%d° name: %s\n", i + 1, names[i]);
+    }
+
+    // Free dynamically allocated memory
+    for (i = 0; i < len; i++) {
+        free(names[i]);
+    }
+    free(names);
+
+    return 0;
 }
