2b1ec95c7513bccf4f27c0b3f4211a7599edf1c6
Eliezer Pimenta <eliezer.a.pimenta@gmail.com>
2023-10-27 13:21:08 -0300

    checks whether a year is a leap year or not

---
+++ leap.c
@@ -0,0 +1,18 @@
+/* checks whether a year is a leap year or not */
+#include <stdio.h>
+
+int main() {
+    int year;
+
+    printf("Leap year checker\n");
+    printf("Enter the year: ");
+    scanf("%d", &year);
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        printf("%d is a leap year.\n", year);
+    } else {
+        printf("%d is not a leap year.\n", year);
+    }
+
+    return 0;
+}
